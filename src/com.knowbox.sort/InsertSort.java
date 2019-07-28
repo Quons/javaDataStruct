ec6497a14414f8baf7f0612c9b74f7cbada7c986
liuyc@knowbox.cn <f103190>
2019-07-28 13:21:34 +0800

    希尔排序

--- src/com.knowbox.sort/InsertSort.java
+++ src/com.knowbox.sort/InsertSort.java
@@ -8,7 +8,7 @@ public class InsertSort {
             arr[i] = (int) (Math.random() * 8000000);
         }*/
         long start = System.currentTimeMillis();
-        int[] arr2 = insertSort(arr);
+        int[] arr2 = insertSort3(arr);
         long end = System.currentTimeMillis();
         System.out.println(end - start);
         System.out.println(Arrays.toString(arr2));
@@ -20,7 +20,7 @@ public class InsertSort {
         for (int i = 1; i < arr.length; i++) {
             //正序遍历
             for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
+                if (arr[i] < arr[j]) {
                     //腾出空位，将i插入
                     tmp = arr[i];
                     for (int f = i - 1; f >= j; f--) {
@@ -29,6 +29,37 @@ public class InsertSort {
                     arr[j] = tmp;
                 }
             }
+        }
+        return arr;
+    }
+
+    public static int[] insertSort2(int[] arr) {
+        int tmp;
+        for (int i = 1; i < arr.length; i++) {//进行插入排序
+            for (int j = i - 1; j >= 0; j -= 1) {//从右至左遍历有序部分，给待插入元素找位置
+                if (arr[j] > arr[j + 1]) {//不断比较当前待插入元素与遍历元素的大小，不断移动，直至待插入元素大于遍历的元素，说明位置正确
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    //将数组分成左边的有序列和右边的无序列，每次从右边的无序列中取出一个插入到有序列中
+    public static int[] insertSort3(int[] arr) {
+        int tmp;
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;//arr[i]为待插入元素
+            tmp = arr[i];//记录待插入的值
+            //后移元素
+            while (j - 1 >= 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j -= 1;
+            }
+            //退出循环说明位置空出来了
+            arr[j] = tmp;
         }
         return arr;
     }
---
+++ src/com.knowbox.sort/ShellSort.java
@@ -0,0 +1,73 @@
+public class ShellSort {
+    public static void main(String[] args) {
+//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000);
+        }
+        long start = System.currentTimeMillis();
+        int[] arr2 = shellSort3(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+       /* for (int a : arr2) {
+            System.out.println(a);
+        }*/
+    }
+
+    //交换法
+    public static int[] shellSort(int[] arr) {
+        int tmp;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {//控制步长
+            for (int i = gap; i < arr.length; i++) {//进行插入排序
+                for (int j = i - gap; j >= 0; j -= gap) {//将右边无序部分的第一个元素插入到左边有序部分的正确位置
+                    if (arr[j] > arr[j + gap]) {//不断比较当前待插入元素与遍历元素的大小，不断移动，直至待插入元素大于遍历的元素，说明位置正确
+                        tmp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = tmp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
+    //依次降低步长，进行宏观调整
+    public static int[] shellSort2(int[] arr) {
+        int tmp;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {//控制步长
+            for (int i = gap; i < arr.length; i++) {
+                //正序遍历
+                for (int j = i % gap; j < i; j += gap) {
+                    if (arr[i] < arr[j]) {
+                        //腾出空位，将i插入
+                        tmp = arr[i];
+                        for (int f = i - gap; f >= j; f -= gap) {
+                            arr[f + gap] = arr[f];
+                        }
+                        arr[j] = tmp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
+    //移位法
+    public static int[] shellSort3(int[] arr) {
+        int tmp;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {//控制步长
+            for (int i = gap; i < arr.length; i++) {//以gap为界，左边为有序组，gap以及gap右边的为无序组
+                int j = i;//i为分界线
+                tmp = arr[i];//记录待插入数值
+                while (j - gap >= 0 && tmp < arr[j - gap]) {//移位，把位置给待插入值腾出位置
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = tmp;//插入
+            }
+        }
+        return arr;
+    }
+}
+
+
